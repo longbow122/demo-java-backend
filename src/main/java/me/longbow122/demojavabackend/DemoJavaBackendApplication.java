aacfa8c21f294d1b8e6f546d62949e9c220e1a94
longbow122 <dhruvilp480@gmail.com>
2025-03-07 00:15:07 +0000

    Implement additional project structure

---
+++ src/main/java/me/longbow122/demojavabackend/DemoJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package me.longbow122.demojavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaBackendApplication.class, args);
+	}
+
+}
